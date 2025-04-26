13941e622f4e7cb515ae09cd0b9f14f982cdd123
jesusalvarado2023 <132959180+jesusalvarado2023@users.noreply.github.com>
2025-04-26 13:15:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ import streamlit as st
 
 def procesar_bloqueador(texto):
     # Elimina [R1] y agrega %11 luego de [Si]
+    st.write("[R1][Si](C1=CC=CC=C1)(C2=CC=CC=C2)C(C)(C)C")
     texto = texto.replace('[R1]', '')
     texto = texto.replace('[Si]', '[Si]%11')
     return texto
 
 def procesar_puente(texto):
     # Elimina [A], reemplaza ([R1]) por %10, agrega %11 luego de N1
+    st.write("[A]N1C2=CC=CC(I)=C2C(C[C@@]3([H])N([R1])CC(C=C3)=O)=C1")
     texto = texto.replace('[A]', '')
     texto = texto.replace('([R1])', '%10')
     texto = texto.replace('N1', 'N1%11')
@@ -15,6 +17,7 @@ def procesar_puente(texto):
 
 def procesar_direccionador(texto):
     # Elimina [A] y agrega %10 después de la primera letra
+    st.write("[A]C(OCC(Cl)(Cl)Cl)=O")
     texto = texto.replace('[A]', '')
     if texto:
         texto = texto[0] + '%10' + texto[1:]
